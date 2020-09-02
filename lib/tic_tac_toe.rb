3ce969ddeed2b8d67a3cbfe8d2f580d7f5b8a5f7
placid-tier-4550 <james.ardery@gmail.com>
2020-09-02 17:54:44 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -37,7 +37,7 @@ def turn(board)
   user_input = gets.strip
   index = input_to_index(user_input)
   if valid_move?(board, index)
-    move(board, index, value)
+    move(board, index, current_player(board))
     display_board(board)
   else
     puts "Please Enter a Valid Move"

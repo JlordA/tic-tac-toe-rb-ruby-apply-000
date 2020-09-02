52c4c14d2105a779b819c991384e83abea971610
placid-tier-4550 <james.ardery@gmail.com>
2020-09-02 16:41:54 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -58,3 +58,44 @@ def current_player(board_array)
     "O"
   end
 end
+
+def won?(board)
+  WIN_COMBINATIONS.find do |combo|
+    board[combo[0]] == board[combo[1]] && board[combo[1]] == board[combo[2]] && position_taken?(board, combo[0])
+  end
+end
+
+def full?(board)
+  board.each_with_index do |slot, index|
+    #binding.pry#!(board[index].nil? || board[index] == " ")
+    if !(position_taken?(board,index))
+      return false
+    end
+  end
+end
+
+def draw?(board)
+  #board.each do |slot|
+    #binding.pry
+  if full?(board) && !(won?(board))
+    true
+  else
+    false
+  end
+end
+
+def over?(board)
+  if won?(board) || draw?(board) || full?(board)
+    true
+  else
+    false
+  end
+end
+
+def winner(board)
+
+  if combo = won?(board)
+    #binding.pry
+    board[combo[0]]
+  end
+end

17b9762a4a3c9592f959a9d27d7541bf478d5da2
placid-tier-4550 <james.ardery@gmail.com>
2020-09-02 17:27:21 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -16,7 +16,7 @@ def input_to_index(user_input)
   user_input.to_i - 1
 end
 
-def move(board, input_to_index, value = X)
+def move(board, input_to_index) #value = X)
   board[input_to_index] = value
 end
 
